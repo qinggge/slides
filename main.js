35ee1265c492c2e01e3426b87fc0c40a06b2ceaa
qinggge <zhu_huang@126.com>
2018-06-17 13:21:49 +0800

    有bug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let n
 初始化()
-setInterval(() => {
+let timer = setInterval(() => {
     makeLeave(getImage(n))
         .one('transitionend',(e) => {
             makeEnter($(e.currentTarget))
@@ -8,6 +8,22 @@ setInterval(() => {
     makeCurrent(getImage(n+1))
     n+=1
 },2000)
+
+document.addEventListener('visibilitychange',function(e){
+    console.log(document.hidden)
+    if(document.hidden){
+        window.clearInterval(timer)
+    }else{
+        timer = setInterval(() => {
+            makeLeave(getImage(n))
+                .one('transitionend',(e) => {
+                    makeEnter($(e.currentTarget))
+                })
+            makeCurrent(getImage(n+1))
+            n+=1
+        },2000)
+    }
+})
 
 function getImage(n){
     return $(`.images > img:nth-child(${x(n)})`)
